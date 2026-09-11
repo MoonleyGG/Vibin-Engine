47fce050a7a22dd53201f1302edf5b27d146f7ab
MoonleyGG <moonley.gg@gmail.com>
2026-09-11 19:42:18 +0200

    shit am i smoking weed or some shit couse last time i checked an flxsprite isnt an flxsound

--- Source/util/SoundUtil.hx
+++ Source/util/SoundUtil.hx
@@ -3,7 +3,22 @@ package util; // im so proud
 import flixel.sound.FlxSound;
 
 class SoundUtil {
-    public static function playMusic(key:String, volume:Float = 1.0, looped:Bool = false, autoDestroy:Bool = true):FlxSound {
-        return FlxG.sound.play("assets/music/" + key + "/music.ogg", volume, looped, null, autoDestroy);
+    public static function playMusic(key:String, volume:Float = 1.0, looped:Bool = false, autoDestroy:Bool = true, persist:Bool = false):FlxSound {
+        var path:String = "assets/music/" + key + "/music.ogg";
+
+        for (sound in FlxG.sound.list.members) {
+            if (sound != null && sound.playing && sound.name == key) {
+                return sound;
+            }
+        }
+
+        var sound:FlxSound = FlxG.sound.play(path, volume, looped, null, autoDestroy);
+
+        if (sound != null) {
+            sound.presist = persist;
+            sound.name = key; // tag it so its findable in the loop next time
+        }
+
+        return sound;
     }
 }
